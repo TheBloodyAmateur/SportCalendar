09c281b89e4a5b173124002b7b50697913a967a9
TheBloodyAmateur <botan_kadir@outlook.com>
2022-01-29 10:35:27 +0100

    Improved connection, server is able to send data to client

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ server.on('connection', socket => {
 
     console.log("New client connected!");
 
+    //Recieving data from the client
     socket.on('message', message => {
         console.log(`Got it, ${message}`);
+        socket.send(`Got message: ${message}`);
     });
 
+    //Message pops up when the client is closing the connection
     socket.on('close', function(){
         console.log('Client closed!');
     });
